0b49e51d43dc7f64701e99974fd15603a183980c
supersymmetry <maildumpert@gmail.com>
2013-01-17 23:34:41 +0100

    init

---
+++ arandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --mode 1280x1024 --pos 0x0 --rotate normal --output DVI-I-1 --mode 1600x900 --pos 1280x0 --rotate normal --output DVI-I-0 --off --output HDMI-0 --off
